163a1d9f304b670264e863e43e258b938dfbf881
Lynda Foster <lfost42@wgu.edu>
2022-03-04 22:05:10 -0800

    added UI

---
+++ BrailleSortUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BrailleSortUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
